6b3a51839b9bf5dbdc9722e31151a14293b1fd7b
Megan Janel Zimmer <megan@theaterconnects.com>
2011-04-12 19:02:01 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'b3088859f8590630cfba5305760dc939a35d9f510dfb02d597a05758a8dff95297140b6a0056379d22da3ee4e9aa28a2113c96ebb3d0d9f9e91bf3efb58ae658'
